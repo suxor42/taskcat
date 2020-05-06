6aff61d9dde71b6af0cb7e88204027b3e3546e6a
Mike Paßberg <mike.passberg@kreuzwerker.de>
2020-05-06 11:14:48 +0200

    Simplify botocache

--- taskcat/_client_factory.py
+++ taskcat/_client_factory.py
@@ -1,13 +1,7 @@
 import logging
-import operator
-from functools import reduce
-from time import sleep
-from typing import Any, Dict, List
+from typing import Dict
 
 import boto3
-import botocore.loaders as boto_loader
-import botocore.regions as boto_regions
-from botocore.config import Config as BotoConfig
 from botocore.exceptions import ClientError, NoCredentialsError, ProfileNotFound
 
 from taskcat.exceptions import TaskCatException
@@ -18,76 +12,42 @@ REGIONAL_ENDPOINT_SERVICES = ["sts"]
 
 
 class Boto3Cache:
-    RETRIES = 10
-    BACKOFF = 2
-    DELAY = 0.1
-    CLIENT_THROTTLE_RETRIES = 20
-
     def __init__(self, _boto3=boto3):
         self._boto3 = _boto3
-        self._session_cache: Dict[str, Dict[str, boto3.Session]] = {}
-        self._client_cache: Dict[str, Dict[str, Dict[str, boto3.client]]] = {}
-        self._resource_cache: Dict[str, Dict[str, Dict[str, boto3.resource]]] = {}
-        self._account_info: Dict[str, Dict[str, str]] = {}
-        self._lock_cache_update = False
+        self._account_info: Dict[str, str] = {}
+        self._region_partition_map = None
+        self._session_cache: Dict[str, boto3.Session] = {}
 
     def session(self, profile: str = "default", region: str = None) -> boto3.Session:
-        region = "NA"  # self._get_region(region, profile)
-        try:
-            session = self._cache_lookup(
-                self._session_cache,
-                [profile, region],
-                self._boto3.Session,
-                [],
-                {"profile_name": profile},
-            )
-        except ProfileNotFound:
-            if profile != "default":
-                raise
-            session = self._boto3.Session(profile_name=profile)
-            self._cache_set(self._session_cache, [profile, region], session)
-        return session
+        return self._session_cache.setdefault(profile, self._boto3.Session(profile_name=profile, region_name=region))
 
     def client(
         self, service: str, profile: str = "default", region: str = None
     ) -> boto3.client:
-        region = self._get_region(region, profile)
-        session = self.session(profile)
-        kwargs = {"config": BotoConfig(retries={"max_attempts": 20})}
-        if service in REGIONAL_ENDPOINT_SERVICES:
-            kwargs.update({"endpoint_url": self._get_endpoint_url(service, region)})
-        return self._cache_lookup(
-            self._client_cache,
-            [profile, region, service],
-            session.client,
-            [service, region],
-            kwargs,
-        )
+        return self.session(profile).client(service_name=service, region_name=region)
 
     def resource(
         self, service: str, profile: str = "default", region: str = None
     ) -> boto3.resource:
-        region = self._get_region(region, profile)
-        session = self.session(profile, region)
-        return self._cache_lookup(
-            self._resource_cache,
-            [profile, region, service],
-            session.resource,
-            [service],
-        )
+        return self.session(profile).resource(service_name=service, region_name=region)
 
     def partition(self, region: str) -> str:
-        return {
-            "cn-north-1": "aws-cn",
-            "us-gov-west-1": "aws-us-gov",
-        }.get(region, "aws")
+        if self._region_partition_map is None:
+            partitions = self._boto3.Session().get_available_partitions()
+            partition_region_map = {}
+            for partition in partitions:
+                partition_region_map[partition] = self._boto3.Session().get_available_regions(
+                    service_name=REGIONAL_ENDPOINT_SERVICES[0], partition_name=partition)
+            self._region_partition_map = {}
+            for partition, regions in partition_region_map.items():
+                for single_region in regions:
+                    self._region_partition_map[single_region] = partition
+        return self._region_partition_map.get(region, "aws")
 
     def account_id(self, profile: str = "default") -> str:
-        return self._cache_lookup(
-            self._account_info, [profile], self._get_account_info, [profile]
-        )["account_id"]
+        return self._account_info.setdefault(profile, self._get_account_info(profile))
 
-    def _get_account_info(self, profile):
+    def _get_account_info(self, profile) -> str:
         session = self.session(profile)
         region = session.region_name
         sts_client = session.client("sts", region_name=region)
@@ -96,105 +56,33 @@ class Boto3Cache:
         except ClientError as e:
             if e.response["Error"]["Code"] == "AccessDenied":
                 raise TaskCatException(
-                    f"Not able to fetch account number from {region} using profile "
+                    f"Not able to fetch account number using profile "
                     f"{profile}. {str(e)}"
                 )
             raise
         except NoCredentialsError as e:
             raise TaskCatException(
-                f"Not able to fetch account number from {region} using profile "
+                f"Not able to fetch account number using profile "
                 f"{profile}. {str(e)}"
             )
         except ProfileNotFound as e:
             raise TaskCatException(
-                f"Not able to fetch account number from {region} using profile "
+                f"Not able to fetch account number using profile "
                 f"{profile}. {str(e)}"
             )
-        return {"account_id": account_id}
-
-    def _make_parent_keys(self, cache: dict, keys: list):
-        if keys:
-            if not cache.get(keys[0]):
-                cache[keys[0]] = {}
-            self._make_parent_keys(cache[keys[0]], keys[1:])
-
-    def _cache_lookup(self, cache, key_list, create_func, args=None, kwargs=None):
-        try:
-            value = self._cache_get(cache, key_list)
-        except KeyError:
-            args = [] if not args else args
-            kwargs = {} if not kwargs else kwargs
-            value = self._get_with_retry(create_func, args, kwargs)
-            self._cache_set(cache, key_list, value)
-        return value
-
-    def _get_with_retry(self, create_func, args, kwargs):
-        retries = self.RETRIES
-        delay = self.DELAY
-        while retries:
-            try:
-                return create_func(*args, **kwargs)
-            except KeyError as e:
-                if str(e) not in ["'credential_provider'", "'endpoint_resolver'"]:
-                    raise
-                backoff = (self.RETRIES - retries + delay) * self.BACKOFF
-                sleep(backoff)
-
-    @staticmethod
-    def _get_endpoint_url(service, region):
-        data = boto_loader.create_loader().load_data("endpoints")
-        endpoint_data = boto_regions.EndpointResolver(data).construct_endpoint(
-            service, region
-        )
-        if not endpoint_data:
-            raise TaskCatException(
-                f"unable to resolve endpoint for {service} in {region}"
-            )
-        return f"https://{service}.{region}.{endpoint_data['dnsSuffix']}"
-
-    @staticmethod
-    def _cache_get(cache: dict, key_list: List[str]):
-        return reduce(operator.getitem, key_list, cache)
-
-    def _cache_set(self, cache: dict, key_list: list, value: Any):
-        self._make_parent_keys(cache, key_list[:-1])
-        self._cache_get(cache, key_list[:-1])[key_list[-1]] = value
-
-    def _get_region(self, region, profile):
-        if not region:
-            region = self.get_default_region(profile)
-        return region
-
-    def _get_partition(self, profile):
-        partition_regions = [
-            ("aws", "eu-west-1"),
-            ("aws-cn", "cn-north-1"),
-            ("aws-us-gov", "us-gov-west-1"),
-        ]
-        for partition, region in partition_regions:
-            try:
-                self.session(profile, region).client(
-                    "sts", region_name=region
-                ).get_caller_identity()
-                return partition, region
-            except ClientError as e:
-                if "InvalidClientTokenId" in str(e):
-                    continue
-                raise
-        raise ValueError("cannot find suitable AWS partition")
+        return account_id
 
     def get_default_region(self, profile_name="default") -> str:
         try:
             session = self._boto3.session.Session(profile_name=profile_name)
             region = session.region_name
-            self._cache_set(self._session_cache, ["NA", region], session)
         except ProfileNotFound:
             if profile_name != "default":
                 raise
             region = self._boto3.session.Session().region_name
         if not region:
-            _, region = self._get_partition(profile_name)
+            region = "us-east-1"
             LOG.warning(
-                "Region not set in credential chain, defaulting to {}".format(region)
+                f"Region not set in credential chain, defaulting to {region}"
             )
         return region
--- tests/test_client_factory.py
+++ tests/test_client_factory.py
@@ -9,93 +9,18 @@ from taskcat.exceptions import TaskCatException
 
 
 class TestBoto3Cache(unittest.TestCase):
-    @mock.patch("taskcat._client_factory.boto3", autospec=True)
-    def test_stable_concurrency(self, mock_boto3):
-        # Sometimes boto fails with KeyErrors under high concurrency
-        for key_error in ["endpoint_resolver", "credential_provider"]:
-            mock_boto3.Session.side_effect = [KeyError(key_error), mock.DEFAULT]
-            c = Boto3Cache(_boto3=mock_boto3)
-            c.session("default")
 
-    @mock.patch("taskcat._client_factory.Boto3Cache._cache_set", autospec=True)
-    @mock.patch("taskcat._client_factory.Boto3Cache._cache_lookup", autospec=True)
-    def test_session_invalid_profile(self, mock_cache_lookup, mock_cache_set):
-        mock_cache_lookup.side_effect = ProfileNotFound(profile="non-existent-profile")
-        cache = Boto3Cache()
-        with self.assertRaises(ProfileNotFound):
-            cache.session(profile="non-existent-profile")
-        self.assertEqual(mock_cache_lookup.called, False)
-        cache._get_region = mock.Mock(return_value="us-east-1")
-        with self.assertRaises(ProfileNotFound):
-            cache.session(profile="non-existent-profile")
-        self.assertEqual(mock_cache_lookup.called, True)
-
-    @mock.patch("taskcat._client_factory.Boto3Cache._get_partition", autospec=True)
-    @mock.patch("taskcat._client_factory.Boto3Cache._cache_set", autospec=True)
-    @mock.patch("taskcat._client_factory.Boto3Cache._cache_lookup", autospec=True)
-    @mock.patch("taskcat._client_factory.boto3.Session", autospec=True)
-    def test_session_no_profile(
-        self, mock_boto3, mock_cache_lookup, mock_cache_set, mock_get_partition
-    ):
-        mock_get_partition.return_value = (None, "us-east-1")
-        mock_cache_lookup.side_effect = ProfileNotFound(profile="non-existent-profile")
-        Boto3Cache().session()  # default value should be "default" profile
-        self.assertEqual(mock_boto3.called, True)
-        self.assertEqual(mock_cache_lookup.called, True)
-        self.assertEqual(mock_cache_set.called, True)
-
-    @mock.patch("taskcat._client_factory.Boto3Cache._get_region", autospec=True)
-    @mock.patch("taskcat._client_factory.Boto3Cache._cache_lookup", autospec=True)
     @mock.patch("taskcat._client_factory.Boto3Cache.session", autospec=True)
-    def test_client(self, mock_session, mock_cache_lookup, mock__get_region):
-        Boto3Cache().client("s3")
-        self.assertEqual(mock_session.called, True)
-        self.assertEqual(mock_cache_lookup.called, True)
-        self.assertEqual(mock__get_region.called, True)
-
-    @mock.patch("taskcat._client_factory.Boto3Cache._get_endpoint_url")
-    @mock.patch("taskcat._client_factory.Boto3Cache._get_region", autospec=True)
-    @mock.patch("taskcat._client_factory.Boto3Cache._cache_lookup", autospec=True)
-    @mock.patch("taskcat._client_factory.Boto3Cache.session", autospec=True)
-    def test_client_sts(
-        self, mock_session, mock_cache_lookup, mock__get_region, mock__get_endpoint_url
-    ):
-        mock__get_endpoint_url.return_value = "https://sts.us-east-1.amazonaws.com"
-        Boto3Cache().client("sts")
-        self.assertEqual(mock_session.called, True)
-        self.assertEqual(mock_cache_lookup.called, True)
-        self.assertEqual(mock__get_region.called, True)
-        self.assertEqual(mock__get_endpoint_url.called, True)
-
-    @mock.patch("taskcat._client_factory.Boto3Cache._get_region", autospec=True)
-    @mock.patch("taskcat._client_factory.Boto3Cache._cache_lookup", autospec=True)
-    @mock.patch("taskcat._client_factory.Boto3Cache.session", autospec=True)
-    def test_resource(self, mock_session, mock_cache_lookup, mock__get_region):
+    def test_resource(self, mock_session):
         Boto3Cache().resource("s3")
         self.assertEqual(mock_session.called, True)
-        self.assertEqual(mock_cache_lookup.called, True)
-        self.assertEqual(mock__get_region.called, True)
-
-    @mock.patch("taskcat._client_factory.Boto3Cache._cache_lookup", autospec=True)
-    def test_partition(self, mock_cache_lookup):
-        Boto3Cache().partition()
-        self.assertEqual(mock_cache_lookup.called, True)
-
-    @mock.patch("taskcat._client_factory.Boto3Cache._cache_lookup", autospec=True)
-    def test_account_id(self, mock_cache_lookup):
-        Boto3Cache().account_id()
-        self.assertEqual(mock_cache_lookup.called, True)
 
     @mock.patch("taskcat._client_factory.boto3", autospec=True)
-    @mock.patch("taskcat._client_factory.Boto3Cache._get_partition", autospec=True)
-    @mock.patch("taskcat._client_factory.Boto3Cache._get_region", autospec=True)
     @mock.patch("taskcat._client_factory.Boto3Cache.session")
     def test__get_account_info(
-        self, mock_session, mock__get_region, mock__get_partition, mock_boto3
+        self, mock_session, mock_boto3
     ):
-        mock__get_region.return_value = "us-east-1"
         session = boto3.Session()
-        session.get_available_regions = mock.Mock()
         session.client = mock.Mock()
         sts = mock.Mock()
         sts.get_caller_identity.return_value = {"Account": "123412341234"}
@@ -103,21 +28,16 @@ class TestBoto3Cache(unittest.TestCase):
         mock_session.return_value = session
         mock_boto3.session.Session = mock.Mock()
         mock_boto3.session.Session.return_value = session
-        cache = Boto3Cache(_boto3=mock_boto3)
+        cache = Boto3Cache()
 
-        mock__get_partition.return_value = ("aws-us-gov", "us-gov-east-1")
-        partition = cache._get_account_info("default")["partition"]
+        partition = cache.partition("us-gov-east-1")
         self.assertEqual(partition, "aws-us-gov")
 
-        mock__get_partition.return_value = ("aws-cn", "cn-north-1")
-        partition = cache._get_account_info("default")["partition"]
+        partition = cache.partition("cn-north-1")
         self.assertEqual(partition, "aws-cn")
 
-        mock__get_partition.return_value = ("aws", "us-east-1")
-        partition = cache._get_account_info("default")["partition"]
+        partition = cache.partition("us-east-1")
         self.assertEqual(partition, "aws")
-
-        self.assertEqual(3, sts.get_caller_identity.call_count)
 
         sts.get_caller_identity.side_effect = ClientError(
             error_response={"Error": {"Code": "test"}}, operation_name="test"
@@ -152,7 +72,7 @@ class TestBoto3Cache(unittest.TestCase):
         mock_session.return_value = session
         mock_boto3.session.Session = mock.Mock()
         mock_boto3.session.Session.return_value = session
-        cache = Boto3Cache(_boto3=mock_boto3)
+        cache = Boto3Cache()
 
         invalid_token_exception = ClientError(
             error_response={"Error": {"Code": "InvalidClientTokenId"}},
@@ -160,18 +80,17 @@ class TestBoto3Cache(unittest.TestCase):
         )
 
         sts.get_caller_identity.side_effect = [True]
-        result = cache._get_partition("default")
-        self.assertEqual(result, ("aws", "us-east-1"))
+        result = cache.partition("us-east-1")
+        self.assertEqual(result, "aws")
 
         sts.get_caller_identity.side_effect = [invalid_token_exception, True]
-        result = cache._get_partition("default")
-        self.assertEqual(result, ("aws-cn", "cn-north-1"))
+        result = cache.partition("cn-north-1")
+        self.assertEqual(result, "aws-cn")
 
         sts.get_caller_identity.side_effect = [
             invalid_token_exception,
             invalid_token_exception,
             True,
         ]
-        result = cache._get_partition("default")
-        self.assertEqual(result, ("aws-us-gov", "us-gov-west-1"))
-        self.assertEqual(sts.get_caller_identity.call_count, 6)
+        result = cache.partition("us-gov-west-1")
+        self.assertEqual(result, "aws-us-gov")
